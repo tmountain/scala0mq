87c954d60f5489524a3e3fadd7b308fdd5742d3d
Travis Whitton <travis.whitton@escapemg.com>
2011-06-21 14:58:17 -0400

    change polling interval

--- asynclient.scala
+++ asynclient.scala
@@ -14,16 +14,17 @@ object asynclient  {
             //  Generate printable identity for the client
             setID(client)
             val identity = new String(client getIdentity)
-            // println(identity)
-            client.connect("tcp://localhost:5570")
+            client.connect("tcp://travis.in.escapemg.com:5570")
             val poller = ctx.poller(1)
-            poller.register(client,ZMQ.Poller.POLLIN)
+            poller.register(client, ZMQ.Poller.POLLIN)
             var requestNbr = 0
+
             while (true) {
-                //  Tick once per second, pulling in arriving messages
+                // Tick once per 1/100th of a second pulling in arriving messages
                 for (centitick <- 1 to 100) {
                     //poller.poll(10000)
-                    poller.poll(100)
+                    //poller.poll(100)
+                    poller.poll
                     if (poller.pollin(0)) {
                         val msg = new ZMsg(client)
                         printf("%s : %s\n", identity, msg.bodyToString)
@@ -38,11 +39,9 @@ object asynclient  {
 
     //  This main thread simply starts several clients, and a server, and then
     //  waits for the server to finish.
-    //
     def main(args : Array[String]) {
         val Nclients = 3
         val clients = List.fill(Nclients)(new Thread(new ClientTask()))
         clients foreach (_.start)
     }
-
 }
