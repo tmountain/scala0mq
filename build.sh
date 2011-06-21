88fd20721a420f2b4722b35ea01c7df3d65093f2
Travis Whitton <travis.whitton@escapemg.com>
2011-06-21 14:42:45 -0400

    moved build scripts

--- build.sh
+++
@@ -1,1 +0,0 @@
-scalac -d classes -classpath ./zmq.jar async.scala  utils.scala ZMsg.scala Dns.scala
---
+++ build_srv.sh
@@ -0,0 +1,1 @@
+scalac -d classes -classpath ./zmq.jar asyncsrv.scala  utils.scala ZMsg.scala Dns.scala
