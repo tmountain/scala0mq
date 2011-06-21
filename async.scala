d8edbb585ae3dce84115883c0ce45cfa3284c7f0
Travis Whitton <travis.whitton@escapemg.com>
2011-06-21 14:37:45 -0400

    moved client to separate class

--- async.scala
+++ async.scala
@@ -1,52 +1,7 @@
-/*
-*  Asynchronous client-to-server (DEALER to BROKER)
-*
-*  While this example runs in a single process, that is just to make
-*  it easier to start and stop the example. Each task has its own
-*  context and conceptually acts as a separate process.
-*
-*  @Author:     Giovanni Ruggiero
-*  @Email:      giovanni.ruggiero@gmail.com
-*/
-
 import org.zeromq.ZMQ
 import ZHelpers._
 
 object asyncsrv  {
-    //  ---------------------------------------------------------------------
-    //  This is our client task
-    //  It connects to the server, and then sends a request once per second
-    //  It collects responses as they arrive, and it prints them out. We will
-    //  run several client tasks in parallel, each with a different random ID.
-    class ClientTask() extends Runnable {
-        def run() {
-            val ctx = ZMQ.context(1)
-            val client = ctx.socket(ZMQ.XREQ)
-            //  Generate printable identity for the client
-            setID(client)
-            val identity = new String(client getIdentity)
-            // println(identity)
-            client.connect("tcp://localhost:5570")
-            val poller = ctx.poller(1)
-            poller.register(client,ZMQ.Poller.POLLIN)
-            var requestNbr = 0
-            while (true) {
-                //  Tick once per second, pulling in arriving messages
-                for (centitick <- 1 to 100) {
-                    //poller.poll(10000)
-                    poller.poll(100)
-                    if (poller.pollin(0)) {
-                        val msg = new ZMsg(client)
-                        printf("%s : %s\n", identity, msg.bodyToString)
-                    }
-                }
-                requestNbr += 1
-                val msg = new ZMsg("request: %d" format requestNbr)
-                client.sendMsg(msg)
-            }
-        }
-    }
-
     //  ---------------------------------------------------------------------
     //  This is our server task
     //  It uses the multithreaded server model to deal requests out to a pool
@@ -122,9 +77,6 @@ object asyncsrv  {
     //  waits for the server to finish.
     //
     def main(args : Array[String]) {
-        //val Nclients = 3
-        //val clients = List.fill(Nclients)(new Thread(new ClientTask()))
-        //clients foreach (_.start)
         new Thread(new ServerTask()).start
     }
 }
---
+++ asynclient.scala
@@ -0,0 +1,48 @@
+import org.zeromq.ZMQ
+import ZHelpers._
+
+object asynclient  {
+    //  ---------------------------------------------------------------------
+    //  This is our client task
+    //  It connects to the server, and then sends a request once per second
+    //  It collects responses as they arrive, and it prints them out. We will
+    //  run several client tasks in parallel, each with a different random ID.
+    class ClientTask() extends Runnable {
+        def run() {
+            val ctx = ZMQ.context(1)
+            val client = ctx.socket(ZMQ.XREQ)
+            //  Generate printable identity for the client
+            setID(client)
+            val identity = new String(client getIdentity)
+            // println(identity)
+            client.connect("tcp://localhost:5570")
+            val poller = ctx.poller(1)
+            poller.register(client,ZMQ.Poller.POLLIN)
+            var requestNbr = 0
+            while (true) {
+                //  Tick once per second, pulling in arriving messages
+                for (centitick <- 1 to 100) {
+                    //poller.poll(10000)
+                    poller.poll(100)
+                    if (poller.pollin(0)) {
+                        val msg = new ZMsg(client)
+                        printf("%s : %s\n", identity, msg.bodyToString)
+                    }
+                }
+                requestNbr += 1
+                val msg = new ZMsg("request: %d" format requestNbr)
+                client.sendMsg(msg)
+            }
+        }
+    }
+
+    //  This main thread simply starts several clients, and a server, and then
+    //  waits for the server to finish.
+    //
+    def main(args : Array[String]) {
+        val Nclients = 3
+        val clients = List.fill(Nclients)(new Thread(new ClientTask()))
+        clients foreach (_.start)
+    }
+
+}
